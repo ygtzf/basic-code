21b946523c4c0eb6f8bf8dcbca326a6bf9edbbd1
ygt <yaogt@suninfo.com>
2017-06-30 22:03:58 +0800

    feature: implememtion of quick sort

--- sort/common.h
+++ sort/common.h
@@ -1,8 +1,12 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <iostream>
+using namespace std;
+
 #include "bubble_sort.h"
 #include "simple_selection_sort.h"
+#include "quick_sort.h"
 
 void sort_print(int* arr, int);
 
---
+++ sort/quick_sort.h
@@ -0,0 +1,5 @@
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+void quick_sort(int *arry, int size);
+#endif
